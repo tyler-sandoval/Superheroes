d51417d9c649b257fb3f456cc22c222b68772ae1
TYLERSANDOVAL\Student <tyler.sandoval@outlook.com>
2018-07-12 14:36:46 -0500

    COMMIT: Created new solution Superheroes and added UI and EF layers; Connected to Superheroes db thru Web.config; added folder to house metadata in EF layer

---
+++ Superheroes.MVC.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Superheroes.MVC.UI.MvcApplication" Language="C#" %>
---
+++ Superheroes.MVC.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Superheroes.MVC.UI.Startup))]
+namespace Superheroes.MVC.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
